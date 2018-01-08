d48e446e02de307fcee04406cbdd5b1ba9e1d4e6
grzegorz jaczewski <grzesiek.jaczewski@gmail.com>
2018-01-08 21:50:24 +0100

    Employee Hierarchy

---
+++ EmployeeEvaluation/EmployeeEvaluation/Models/EmployeeHierarchyModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeEvaluation.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public bool IsManager { get; set; }
+        public int TeamId { get; set; }
+        public int PositionId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int ManagerId { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+
+    public class Position
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
