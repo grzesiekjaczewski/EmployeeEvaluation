9df8bdcf023f7e0d5132a351ae7509da8063b061
Katarzyna Jaczewska <kaska.jaczewska@hotmail.com>
2018-10-19 13:55:05 +0200

    konto - język

--- EmployeeEvaluation/EmployeeEvaluation/Models/AccountViewModels.cs
+++ EmployeeEvaluation/EmployeeEvaluation/Models/AccountViewModels.cs
@@ -55,10 +55,10 @@ namespace EmployeeEvaluation.Models
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Zapamiętaj mnie")]
         public bool RememberMe { get; set; }
     }
 
@@ -80,7 +80,7 @@ namespace EmployeeEvaluation.Models
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Potwierdzenie hasła")]
         [Compare("Password", ErrorMessage = "Hasło i potwierdzenie hasła nie zgadzają się.")]
         public string ConfirmPassword { get; set; }
     }
@@ -93,14 +93,14 @@ namespace EmployeeEvaluation.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} musi mieć przynajmniej {2} znaków.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Potwierdzenie hasła")]
+        [Compare("Password", ErrorMessage = "Hasło i potwierdzenie hasła nie zgadzają się.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
