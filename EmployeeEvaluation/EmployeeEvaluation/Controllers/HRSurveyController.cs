f56df832be8dd05bf6f8599d4321bb722aeb7cab
grzesiekjaczewski <grzesiek.jaczewski@gmail.com>
2018-11-25 20:24:55 +0000

    Acceptance by HR Manager

--- EmployeeEvaluation/EmployeeEvaluation/Controllers/HRSurveyController.cs
+++ EmployeeEvaluation/EmployeeEvaluation/Controllers/HRSurveyController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using EmployeeEvaluation.Logic;
 using EmployeeEvaluation.Logic.PrepareView;
+using EmployeeEvaluation.Logic.SaveData;
 using EmployeeEvaluation.Models;
 
 namespace EmployeeEvaluation.Controllers
@@ -25,21 +26,66 @@ namespace EmployeeEvaluation.Controllers
             return View(view);
         }
 
-        // GET: HRSurvey/Details/5
-        public ActionResult Details(int? id)
+        // GET: HRSurvey/AcceptSurvey/5
+        public ActionResult AcceptSurvey(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Survey survey = db.T_Survey.Find(id);
-            if (survey == null)
+
+            IPrepareExtendedView<BrowseSurvey, int?> prepareStartSurveyView = new PrepareSurveyBrowseView<BrowseSurvey, int?>();
+            prepareStartSurveyView.Parameters = id;
+            BrowseSurvey browseSurvey = prepareStartSurveyView.GetView(db);
+
+            List<Employee> employees = db.T_Employees.Where(i => i.Id == browseSurvey.Survey.EmployeeId).ToList();
+            ViewBag.UserInfo = employees[0].FirstName + " " + employees[0].LastName;
+
+            if (browseSurvey == null)
             {
                 return HttpNotFound();
             }
-            return View(survey);
+            return View(browseSurvey);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AcceptSurvey()
+        {
+            var hrManagerComment = Request["hrManagerComment"];
+            var id = Request["Survey.Id"];
+            if (ModelState.IsValid)
+            {
+                Tuple<string, string> tuple = new Tuple<string, string>(id, hrManagerComment);
+                ISaveModel<Tuple<string, string>> saveSurvey = new SaveSurveyHRAccept<Tuple<string, string>>();
+                saveSurvey.Save(tuple, db);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: HRSurvey/ViewSurvey/5
+        public ActionResult ViewSurvey(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            IPrepareExtendedView<BrowseSurvey, int?> prepareStartSurveyView = new PrepareSurveyBrowseView<BrowseSurvey, int?>();
+            prepareStartSurveyView.Parameters = id;
+            BrowseSurvey browseSurvey = prepareStartSurveyView.GetView(db);
+
+            List<Employee> employees = db.T_Employees.Where(i => i.Id == browseSurvey.Survey.EmployeeId).ToList();
+            ViewBag.UserInfo = employees[0].FirstName + " " + employees[0].LastName;
+
+            if (browseSurvey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(browseSurvey);
+        }
 
         protected override void Dispose(bool disposing)
         {
---
+++ EmployeeEvaluation/EmployeeEvaluation/Logic/SaveData/SaveSurveyHRAccept.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeEvaluation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeEvaluation.Logic.SaveData
+{
+    public class SaveSurveyHRAccept<T> : ISaveModel<T> where T : class
+    {
+        public void Save(T model, ApplicationDbContext db)
+        {
+            var surveyUserData = model as Tuple<string, string>;
+            Survey survey = db.T_Survey.Find(StringToValue.ParseInt(surveyUserData.Item1));
+            survey.HRSummary = surveyUserData.Item2;
+            survey.SurveyStatusId = 4;
+            db.Entry(survey).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
