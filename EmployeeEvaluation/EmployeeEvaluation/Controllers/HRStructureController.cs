e7d98355d6a4b54985301e501a25dcc0204e4465
grzesiekjaczewski <grzesiek.jaczewski@gmail.com>
2018-11-26 22:06:19 +0000

    Structure step 1

---
+++ EmployeeEvaluation/EmployeeEvaluation/Controllers/HRStructureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeEvaluation.Controllers
+{
+    public class HRStructureController : Controller
+    {
+        // GET: HRStructure
+        public ActionResult Structure()
+        {
+            return View();
+        }
+    }
+}
