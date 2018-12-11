9e1927a832d231ae313dc44539e7ff65d40ea9bb
grzesiekjaczewski <grzesiek.jaczewski@gmail.com>
2018-12-11 22:10:55 +0000

    Structure lines

--- EmployeeEvaluation/EmployeeEvaluation/Scripts/myStructure.js
+++ EmployeeEvaluation/EmployeeEvaluation/Scripts/myStructure.js
@@ -2,6 +2,8 @@
 var context;
 var levelX;
 var levelY;
+var manager;
+var parentY = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function myStructure(structure) {
 
@@ -9,14 +11,10 @@ function myStructure(structure) {
     context = canvas.getContext('2d');
     levelX = 0;
     levelY = 0;
+    y1 = 0;
 
-    context.canvas.height = (structure.TeamCount * 80 + 10) + (structure.PersonCount * 80 + 10);
-    myPerson(structure.Boss, 0);
-
-    //context.beginPath();
-    //context.moveTo(0, 0);
-    //context.lineTo(100, 100);
-    //context.stroke();
+    context.canvas.height = structure.TeamCount * 80 + 10 + (structure.PersonCount * 80 + 10);
+    myPerson(structure.Boss);
 }
 
 function myPerson(person) {
@@ -24,11 +22,11 @@ function myPerson(person) {
             person.LastName,
             person.Position
     );
-
+    parentY[levelX] = levelY;
     levelX++;
+    manager = person.FirstName + " " + person.LastName;
     person.Teams.forEach(myTeams);
     levelX--;
-
 }
 
 function myTeams(team) {
@@ -38,8 +36,29 @@ function myTeams(team) {
     context.font = "20px Arial";
     context.fillStyle = "black";
     context.fillText(team.Name, x + 10, y + 40);
+
+    context.font = "18px Arial bold italic";
+    context.fillText(manager, x + 10, y + 63);
+    context.fillStyle = "black";
     levelY++;
+
+    context.strokeStyle = "navy";
+    context.beginPath();
+    context.moveTo(x - 150, parentY[levelX - 1] * 80 - 10);
+    context.lineTo(x - 150, y + 50);
+    context.lineTo(x - 10, y + 50);
+    context.stroke();
+
+    //var prevX = levelX;
+    //var prevY = levelY;
+
     team.Persons.forEach(myPerson);
+
+    //context.strokeStyle = "#FF0000";
+    //context.beginPath();
+    //context.moveTo(prevX * 250 - 50, prevY * 80 + 10);
+    //context.lineTo(levelX * 250 - 50, (levelY) * 80 + 10);
+    //context.stroke();
 }
 
 
@@ -51,14 +70,16 @@ function drawPerson(firstName, lastName, position) {
     context.fillStyle = "lightblue";
     context.strokeStyle = "grey";
     context.lineWidth = "1";
-    context.fillRect(x, y, 180, 60);
-    context.strokeRect(x, y, 180, 60);
+    context.fillRect(x, y, 280, 48);
+    context.strokeRect(x, y, 280, 48);
+
+    context.font = "14px Arial bold italic";
+    context.fillStyle = "black";
+    context.fillText(position, x + 10, y + 20);
 
     context.font = "15px Arial";
     context.fillStyle = "black";
-    context.fillText(position, x + 10, y + 20);
-    context.fillText(firstName, x + 10, y + 35);
-    context.fillText(lastName, x + 10, y + 50);
+    context.fillText(firstName + " " + lastName, x + 10, y + 38);
 
     levelY++;
 }
